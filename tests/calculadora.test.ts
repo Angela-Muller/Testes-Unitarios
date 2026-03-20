747e5a6895cb11903845bee2a6581ff430c17b4b
ARTHUR ANDRE MULLER <70184433428@senacrs.edu.br>
2026-03-20 14:35:41 -0300

    realese ese

---
+++ tests/calculadora.test.ts
@@ -0,0 +1,25 @@
+import { soma } from "../src/calculadora";
+
+test("deve somar dois números corretamente", () => {
+  expect(soma(2, 3)).toBe(5);
+});
+
+test("deve somar dois números corretamente", () => {
+  expect(soma(2, 3)).toBe(5);
+});
+
+test("deve somar dois números corretamente", () => {
+  expect(soma(2, 3)).toBe(5);
+});
+
+test("deve somar dois números corretamente", () => {
+  expect(soma(2, 3)).toBe(5);
+});
+
+test("deve somar dois números corretamente", () => {
+  expect(soma(2, 3)).toBe(5);
+});
+
+test("deve somar dois números corretamente", () => {
+  expect(soma(2, 3)).toBe(5);
+});
